b6511493656bbe9bba85b191402fa410ebb192fa
microdrigu <michael.rodriguez7@icloud.com>
2018-12-20 14:49:43 -0800

    adding server side fixes for react router 404 responses

--- client/src/pages/Portfolio/Portfolio.js
+++ client/src/pages/Portfolio/Portfolio.js
@@ -6,9 +6,20 @@ class Portfolio extends Component {
     render() {
       return (
         <div className="container body">
+          <div className="row jumbo">
+            <div className="col-md">
+              <h1 className="display-3 text-white">Michael Rodriguez</h1>
+              <p className="lead text-white">Full-Stack Web Developer</p>
+              <p className="lead text-white">Berkeley, California</p>
+            </div>
+          </div>
+          
           <div className="row">
             <div className="col-md">
-              <p>This is what little I have worked on *smile* ":)"</p>
+              here would be a picture of a website
+            </div>
+            <div className="col-md">
+              and here would be a description?
             </div>
             
           </div>
